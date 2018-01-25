90af6b92540a6e76588bc8a2f126dc80afea3f52
R3xKyle <kyle@ringler.com>
2018-01-24 18:31:32 -0800

    Initial player control script

---
+++ 371GameDesign/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float jumpingSpeed;
+    private Rigidbody2D rb2d;
+    private bool grounded;
+
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        grounded = true;
+	}
+	
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        float horizontalSpeed = Input.GetAxis("Horizontal");
+
+        // Player must be grounded in order to be able to jump
+        if (Input.GetKeyDown("space") && grounded)
+        {
+            rb2d.AddForce(new Vector2(horizontalSpeed, jumpingSpeed) * speed);
+        }
+        else
+        {
+            rb2d.AddForce(new Vector2(horizontalSpeed, 0f) * speed);
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision col)
+    {
+        // This requires our objects that are on the ground to have a 'Ground' tag
+        if (col.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
+}
