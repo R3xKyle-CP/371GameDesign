cc8d54fb2ebc4067a3b36507f4ea54108e061021
R3xKyle <kyle@ringler.com>
2018-01-28 13:32:07 -0800

    Added grounded jumping only and simple camera script.

--- 371GameDesign/Assets/Scripts/PlayerController.cs
+++ 371GameDesign/Assets/Scripts/PlayerController.cs
@@ -34,7 +34,7 @@ public class PlayerController : MonoBehaviour {
 
     }
 
-    void OnCollisionEnter2D(Collision col)
+    void OnCollisionEnter2D(Collision2D col)
     {
         // This requires our objects that are on the ground to have a 'Ground' tag
         if (col.gameObject.tag == "Ground")
